1661995db04f8f688b1b3e4c370cc871f6766cbd
Eduard Vasilache <eduard.vasilache@mready.net>
2017-10-05 14:34:49 +0300

    Fixed Intent NullPointerException.

--- hover/src/main/java/net/mready/hover/HoverService.java
+++ hover/src/main/java/net/mready/hover/HoverService.java
@@ -86,21 +86,23 @@ public class HoverService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
 
-        switch (intent.getAction()) {
-            case ACTION_SHOW:
-                //noinspection unchecked
-                addWindow(intent.getIntExtra(EXTRA_WINDOW_ID, 0),
-                        (Class<? extends HoverWindow>) intent.getSerializableExtra(EXTRA_WINDOW_CLASS),
-                        intent.getBundleExtra(EXTRA_ARGUMENTS));
-                break;
+        if (intent != null && intent.getAction() != null) {
+            switch (intent.getAction()) {
+                case ACTION_SHOW:
+                    //noinspection unchecked
+                    addWindow(intent.getIntExtra(EXTRA_WINDOW_ID, 0),
+                            (Class<? extends HoverWindow>) intent.getSerializableExtra(EXTRA_WINDOW_CLASS),
+                            intent.getBundleExtra(EXTRA_ARGUMENTS));
+                    break;
 
-            case ACTION_CLOSE:
-                removeWindow(intent.getIntExtra(EXTRA_WINDOW_ID, 0));
-                break;
+                case ACTION_CLOSE:
+                    removeWindow(intent.getIntExtra(EXTRA_WINDOW_ID, 0));
+                    break;
 
-            case ACTION_CLOSE_ALL:
-                removeAllWindows();
-                break;
+                case ACTION_CLOSE_ALL:
+                    removeAllWindows();
+                    break;
+            }
         }
 
         return START_NOT_STICKY;
@@ -260,26 +262,28 @@ public class HoverService extends Service {
     private class ScreenReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            switch (intent.getAction()) {
-                case Intent.ACTION_SCREEN_OFF:
-                    for (HoverWindow window : windows.values()) {
-                        if (window.attached) {
-                            window.performStop();
+            if (intent != null && intent.getAction() != null) {
+                switch (intent.getAction()) {
+                    case Intent.ACTION_SCREEN_OFF:
+                        for (HoverWindow window : windows.values()) {
+                            if (window.attached) {
+                                window.performStop();
+                            }
                         }
-                    }
-                    break;
-                case Intent.ACTION_SCREEN_ON:
-                    if (keyguardManager.inKeyguardRestrictedInputMode()) {
                         break;
-                    }
-                    //fallthrough
-                case Intent.ACTION_USER_PRESENT:
-                    for (HoverWindow window : windows.values()) {
-                        if (window.attached && !window.started) {
-                            window.performStart();
+                    case Intent.ACTION_SCREEN_ON:
+                        if (keyguardManager.inKeyguardRestrictedInputMode()) {
+                            break;
                         }
-                    }
-                    break;
+                        //fallthrough
+                    case Intent.ACTION_USER_PRESENT:
+                        for (HoverWindow window : windows.values()) {
+                            if (window.attached && !window.started) {
+                                window.performStart();
+                            }
+                        }
+                        break;
+                }
             }
         }
     }
